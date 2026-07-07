df2bd77fe4688d373e8dbef7ee0da7a8268129f7
choinaknakzz <choinaknakzz@gmail.com>
2026-07-07 17:23:19 +0900

    Keep service alive on Telegram polling stderr

--- scripts/run-service.ps1
+++ scripts/run-service.ps1
@@ -11,8 +11,13 @@ try {
 
   "[$(Get-Date -Format o)] Starting GoldbitAutomationLab service with $node" | Out-File -FilePath $logPath -Append -Encoding utf8
   Set-Location $projectRoot
+
+  $previousErrorActionPreference = $ErrorActionPreference
+  $ErrorActionPreference = "Continue"
   & $node $entrypoint dev >> $logPath 2>&1
   $exitCode = $LASTEXITCODE
+  $ErrorActionPreference = $previousErrorActionPreference
+
   "[$(Get-Date -Format o)] GoldbitAutomationLab service exited with code $exitCode" | Out-File -FilePath $logPath -Append -Encoding utf8
   exit $exitCode
 } catch {
